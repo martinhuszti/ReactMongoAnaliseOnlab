10c1a1149d31db8d299f0220f0f0a9e803c661f6
Martin Huszti <martinhuszti@gmail.com>
2019-02-05 22:41:47 +0100

    InitialCommit2

---
+++ src/main/java/com/huszti/gema/analiseresponsiveweb/AnaliseResponsiveWebApplication.java
@@ -0,0 +1,14 @@
+package com.huszti.gema.analiseresponsiveweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseResponsiveWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnaliseResponsiveWebApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/huszti/gema/analiseresponsiveweb/web/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.huszti.gema.analiseresponsiveweb.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
+    }
+}
