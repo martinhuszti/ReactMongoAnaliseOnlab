e1ba71ca0f95b8014def47d83672fb9ed4300f68
Martin Huszti <martinhuszti@gmail.com>
2019-02-14 16:56:14 +0100

    React

--- src/main/java/com/huszti/gema/analiseresponsiveweb/web/HelloWorldController.java
+++ src/main/java/com/huszti/gema/analiseresponsiveweb/web/HelloWorldController.java
@@ -1,12 +1,14 @@
 package com.huszti.gema.analiseresponsiveweb.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping("/")
+    @GetMapping("/api/hello")
     public String sayHello() {
-        return "Hello "  + "!";
+        return "Hello, the time at the server is now " + new Date() + "\n";
     }
 }
