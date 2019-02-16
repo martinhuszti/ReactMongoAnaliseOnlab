8e19eaf198f19e891477cdfceee3ab94e243517f
gszasza1 <gszasza1@gmail.com>
2019-02-16 22:25:03 +0100

    Added Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
 
     state = {};
 
     componentDidMount() {
         setInterval(this.hello, 250);
+        document.title = "Login Page";
     }
 
     hello = () => {
@@ -21,16 +24,19 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">{this.state.message}</h1>
                 </header>
-                <p className="App-intro">
+                <p className="App-intro">F
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
             </div>
         );
     }
 }
+
+
 
 export default App;
