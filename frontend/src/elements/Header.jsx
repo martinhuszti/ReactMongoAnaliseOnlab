3d58c09e3b5234300a1b51a028c7b851b0870b88
gszasza1 <gszasza1@gmail.com>
2019-02-28 20:14:59 +0100

    Rooter

--- frontend/src/elements/Header.jsx
+++ frontend/src/elements/Header.jsx
@@ -1,51 +1,48 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './menu.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import News from './News';
 
+const Home = () => (
+    <News/>
+  );
 
+const Header = () => (
+    <Router>
+    <div>
+        <nav className="header">
+            <div className="container-fluid">
+                <ul id="largemenu" className="row">
+                    <li className="col menuitem padding_menu">
+                        <Link to="/">Hírek</Link>
+                    </li>
+                    <li className="col menuitem padding_menu">
+                        <Link to="/News">Számonkérés</Link>
+                    </li>
+                    <li className="col menuitem padding_menu">
+                        <Link to="/">Információ</Link>
+                    </li>
+                    <li className="col menuitem padding_menu">
+                        <Link to="/"> Feladatok</Link>
+                    </li>
+                    <li className="col menuitem padding_menu">
+                        <Link to="/"> Követelmény</Link>
+                    </li>
+                    <li id="loginitem" className="col menuitem">
+                        <ul id="login" className="padding_menu">
+                            <Link to="/">Profil</Link>
+                        </ul>
 
-//import { Nav, NavItem, NavLink } from 'reactstrap';
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
-class Header extends Component {
-
-
-    render() {
-        return (
-
-
-            
-            <nav className="header">
-                <div className="container-fluid">
-                    <ul id="largemenu" className="row">
-                        <li className="col menuitem padding_menu">
-                            <a href>Hírek</a>
-                        </li>
-                        <li className="col menuitem padding_menu">
-                            <a>Számonkérés</a>
-                        </li>
-                        <li className="col menuitem padding_menu">
-                            <a>Információ</a>
-                        </li>
-                        <li className="col menuitem padding_menu">
-                            <a> Feladatok</a>
-                        </li>
-                        <li className="col menuitem padding_menu">
-                            <a> Követelmény</a>
-                        </li>
-                        <li id="loginitem" className="col menuitem">
-                            <ul id="login" className="padding_menu">
-                                <a>Profil</a>
-                            </ul>
-
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-
-
-        );
-    }
-}
-
+        <Route exact path="/News" component={() => <p style={{marginTop: '200px'}}>asdasdasdasd</p>} />
+        <Route exact path="/" component={Home} />
+     </div>
+    </Router>
+);
 
 export default Header;
---
+++ frontend/src/elements/News.jsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './news.css';
+import React, { Component } from 'react';
+
+const News = () => (
+<div id="placeholder_news">
+<div id= "placeholder_header"></div>
+   <div id="asd">
+
+           <div class="news news_head">
+               <a class="news_text" href="">HATALMAS PLACHOLDER, hatalmas placeholder</a>
+           </div>
+           <div class="news news_body">
+           </div>
+       </div>
+       </div>
+  );
+
+  export default News;
