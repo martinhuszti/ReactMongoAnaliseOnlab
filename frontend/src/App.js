aadfa4216a1a5f901f3c0b6e6e7cff765e252ce4
Martin <martinhuszti@gmail.com>
2019-04-30 16:03:53 +0200

    Updated package.json pt1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Header from './elements/header.jsx';
@@ -18,9 +18,9 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <Header/>
+                <Header />
 
-                <Footer/>
+                <Footer />
             </div>
         );
     }
