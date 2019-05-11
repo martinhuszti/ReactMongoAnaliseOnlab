95cd6d4b721e6986a21dc5a9a3f00a7a4e245289
gszasza1 <gszasza1@gmail.com>
2019-05-11 17:52:40 +0200

    fck Codacy

--- frontend/src/elements/UserMenu/2_results.jsx
+++ frontend/src/elements/UserMenu/2_results.jsx
@@ -70,7 +70,7 @@ class LoggedIn_Result extends Component {
                             <span>Pont</span>
                             <span>Jegy</span>
                         </li>
-                       {record.map(exam => (record.length>0) ?
+                       {record.map((exam) => (record.length>0) ?
                             <li className="result_font" key={exam.id}>
                                 <span>
                                     {exam.type}
