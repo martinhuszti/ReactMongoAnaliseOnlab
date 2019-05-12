6bbd5d7d8e2cccd9979cdb6787754515f5126323
Martin Huszti <martinhuszti@gmail.com>
2019-05-12 23:55:23 +0200

    Codacy Fix part 13

--- src/test/java/com/huszti/gema/analiseresponsiveweb/AnaliseResponsiveWebApplicationTests.java
+++ src/test/java/com/huszti/gema/analiseresponsiveweb/AnaliseResponsiveWebApplicationTests.java
@@ -1,6 +1,5 @@
 package com.huszti.gema.analiseresponsiveweb;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,11 +7,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AnaliseResponsiveWebApplicationTests {
-
-    @Test
-    public void contextLoads() {
-        assert(true);
-    }
 
 }
 
